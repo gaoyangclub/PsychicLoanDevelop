31420410bf1b494eb1be0f95035428af2c92c822
Gao Yang <ygao.hz@best-inc.com>
2017-11-16 15:17:13 +0800

    项目创建

---
+++ PsychicLoan/Pods/Target Support Files/Pods-PsychicLoan/Pods-PsychicLoan-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PsychicLoan : NSObject
+@end
+@implementation PodsDummy_Pods_PsychicLoan
+@end
---
+++ PsychicLoan/PsychicLoan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PsychicLoan
+//
+//  Created by admin on 2017/11/16.
+//  Copyright © 2017年 GaoYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
