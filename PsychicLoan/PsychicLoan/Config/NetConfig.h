7aab22a5a38c54995b26cd8a95f40eccae9f4079
Gao Yang <ygao.hz@best-inc.com>
2017-12-20 18:32:53 +0800

    支持https

--- PsychicLoan/PsychicLoan/Config/NetConfig.h
+++ PsychicLoan/PsychicLoan/Config/NetConfig.h
@@ -9,8 +9,9 @@
 #import <Foundation/Foundation.h>
 
 #define SERVER_URL_TEST @"http://47.100.15.41:8080"//测试环境
+#define SERVER_URL_RELEASE @"https://www.psychicloan.com:8443"//生产环境
 
-#define SERVER_DRIVER_URL SERVER_URL_TEST//[NetConfig getDriverNetUrl:NET_MODE]
+#define SERVER_DRIVER_URL SERVER_URL_RELEASE//[NetConfig getDriverNetUrl:NET_MODE]
 
 #define HOME_POP_VIEW_URL ConcatStrings(SERVER_DRIVER_URL,@"/loan-api/home/getview")
 
